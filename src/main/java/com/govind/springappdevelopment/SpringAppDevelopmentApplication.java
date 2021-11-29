d9e992355b347418e6cc34636df814040454b412
govind6498 <govindsingh6498@gail.com>
2021-11-29 15:49:48 +0530

    ADDED:To initialize project structure

---
+++ src/main/java/com/govind/springappdevelopment/SpringAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.govind.springappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppDevelopmentApplication.class, args);
+	}
+
+}
