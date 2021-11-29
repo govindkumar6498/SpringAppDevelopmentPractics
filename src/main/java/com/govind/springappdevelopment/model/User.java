0e1eea639933bf5e1ed3586ce184713e51a5a488
govind6498 <govindsingh6498@gail.com>
2021-11-29 23:15:19 +0530

    ADDED:To Add user firstName and lastName

---
+++ src/main/java/com/govind/springappdevelopment/model/User.java
@@ -0,0 +1,18 @@
+package com.govind.springappdevelopment.model;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
